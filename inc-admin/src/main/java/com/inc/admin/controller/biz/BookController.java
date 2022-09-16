32e610ed026ef8ac40c0b4cc0be7caa204e3e925
wangying@ctdigitech.com <wangying@ctdigitech.com>
2022-09-16 16:57:22 +0800

    重构图书管理接口实现

--- inc-admin/src/main/java/com/inc/admin/controller/biz/BookController.java
+++ inc-admin/src/main/java/com/inc/admin/controller/biz/BookController.java
@@ -5,6 +5,10 @@ import com.inc.admin.service.biz.BookService;
 import com.inc.admin.utils.R;
 import javax.annotation.Resource;
 import javax.validation.constraints.NotNull;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 */
 @RestController
 @RequestMapping("/book")
+@Api(tags = "图书管理接口")
 public class BookController {
     @Resource
     private BookService bookService;
@@ -22,6 +27,7 @@ public class BookController {
      * 分页查询 图书列表
      */
     @PostMapping("/listByPage")
+    @ApiOperation(value = "图书列表接口")
     public R listByPage(@RequestBody Book req) {
         return R.ok().put("page", bookService.listByPage(req));
     }
@@ -30,6 +36,7 @@ public class BookController {
      * 添加 图书信息
      */
     @PostMapping("/insert")
+    @ApiOperation(value = "图书新增接口", authorizations = {@Authorization(value = "token")})
     public R insert(@RequestBody Book req) {
         return R.operate(bookService.insert(req)>0);
     }
@@ -38,6 +45,7 @@ public class BookController {
      * 更新 图书信息
      */
     @PostMapping("/update")
+    @ApiOperation(value = "图书编辑列表接口")
     public R update(@RequestBody Book req) {
         return R.operate(bookService.update(req)>0);
     }
@@ -46,6 +54,7 @@ public class BookController {
      * 删除 图书信息
      */
     @PostMapping("/delete")
+    @ApiOperation(value = "图书删除接口")
     public R delete(@Validated @NotNull(message = "编号不能为空") @RequestParam("id") @RequestBody Integer id) {
         return R.operate(bookService.delete(id)>0);
     }
